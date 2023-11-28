93812b9975d304ff8b795f0d1c2fceeede254b10
Christian Blöbaum <119411110+Hubi-Muc@users.noreply.github.com>
2023-11-28 07:16:06 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -3,7 +3,7 @@
 REM https://wiki.securepoint.de/AV/Installation/AutomatisierteInstallation
 REM Installationsscript für SophosClient
 
-tasklist /nh /fi "imagename eq guardxservice_x64.exe" | find /i "guardxservice_x64.exe" >nul && (
+tasklist /nh /fi "imagename eq Sophos UI.exe" | find /i "Sophos UI.exe" >nul && (
 exit
 ) || (
 start /wait msiexec /l* "[Pfad\zur\Datei]\setuplog_%COMPUTERNAME%.txt" /i "SophosSetup.exe --products=antivirus,intercept --language=1031 --quiet"
